427ffbb8721a09d6a975d2cc3834cbab97936ba5
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-31 22:58:45 +0600

    Setup mongoose-express-cors-perser

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Database connect successfully");
+
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("Failed to connect database");
+  }
+}
+main();
