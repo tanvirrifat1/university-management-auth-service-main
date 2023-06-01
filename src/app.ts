06935e5d222c386e4da0406580828aa1633e560a
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-06-01 10:00:48 +0600

    added more

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express());
-app.use(express.urlencoded({ extended: true }));
+app.use(express())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("welcome to university management project");
-});
+app.get('/', (req: Application, res: Response) => {
+  res.send('welcome to university management project')
+})
 
-export default app;
+export default app
