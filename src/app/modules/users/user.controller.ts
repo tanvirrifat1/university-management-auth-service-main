3817bc526401bb51a911a41e2fb048c0f0379ead
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-06-09 23:55:14 +0600

    implement Zod error handler

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,21 +1,8 @@
 import { RequestHandler } from 'express'
 import { UserService } from './user.service'
-import { z } from 'zod'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
-    // req-validation
-    const createUserZodSchema = z.object({
-      body: z.object({
-        role: z.string({
-          required_error: 'role is required',
-        }),
-        password: z.string().optional(),
-      }),
-    })
-
-    await createUserZodSchema.parseAsync(req)
-
     const { user } = req.body
     console.log(user)
     const result = await UserService.createUser(user)
